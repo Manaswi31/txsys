7651f0393bb760ce26ebf35b8ef0d81b42c0e5be
jiangbei.wei <jiangbei.wei@31749a9c-224c-0410-b4e1-a76a6b973bc7>
2008-06-09 04:08:58 +0000

    directory 'base' added.

---
+++ ubiquitous/omnetpp/base/SimpleSource.cc
@@ -0,0 +1,64 @@
+
+#include <vector>
+#include <omnetpp.h>
+#include "packet_m.h"
+#include "SimpleSource.h"
+
+
+// Generates traffic for the network.
+Define_Module(SimpleSource);
+
+void SimpleSource::initialize()
+{
+  numSent = 0;
+
+  WATCH(pkSize);
+  WATCH(iaTime);
+  WATCH(numSent);
+
+   // Schedule an event for sending the first packet.
+  timeToGenerateAPacket = new cMessage("timeToGenerateAPacket");
+  iaTime = par("iaTime");
+  scheduleAt(iaTime, timeToGenerateAPacket);
+}
+
+void SimpleSource::handleMessage(cMessage *msg)
+{
+  // Time to create and send a packet.
+  if (ev.isGUI()) parentModule()->bubble("Sending a packet");
+    numSent++;
+
+  pkSize = par("pkSize");
+
+  // Display the number of packets sent.
+  char buf[40];
+  sprintf(buf, "sent: %ld", numSent);
+  displayString().setTagArg("t", 0, buf);
+
+  // Prepare a packet.
+  // Application layer does not know the node address yet (hence -1)
+  /*
+  char pkname[40];
+  sprintf(pkname,"pk-(%d)-to-%d-#%ld", -1, destAddress, numSent);
+  ev << simTime() << " generating a packet " << pkname << endl;
+  Packet *pk = new Packet(pkname);
+  */
+  Packet *pk = new Packet();
+  pk->setLength(pkSize);
+  pk->setTimestamp();
+  send(pk,"out");
+
+  // Schedule an event for sending the next packet.
+  iaTime = par("iaTime");
+  scheduleAt(simTime()+iaTime, timeToGenerateAPacket);
+}
+
+void SimpleSource::finish()
+{
+  ev << parentModule()->fullName() << "." << fullName() << ":" << endl;
+  ev << "  pks. sent:     " << numSent << endl;
+  ev << " " << endl;
+
+  recordScalar("#sent", numSent);
+}
+
---
+++ ubiquitous/omnetpp/base/Sink.cc
@@ -0,0 +1,75 @@
+
+
+#include <vector>
+#include <omnetpp.h>
+#include "packet_m.h"
+#include "Sink.h"
+
+Define_Module(Sink);
+
+void Sink::initialize()
+{
+  numReceived = 0;
+
+  WATCH(numReceived);
+
+  hopCountVector.setName("hopCountVector");
+  hopCountStats.setName("hopCountStats");
+  hopCountStats.setRangeAutoUpper(0,20,1.5);
+
+  pkDelayVector.setName("pkDelayVector");
+  pkDelayStats.setName("pkDelayStats");
+  // pkDelayStats.setRangeAutoUpper(0,20,1.5);
+
+  pkSizeStats.setName("pkSizeStats");
+  // pkSizeStats.setRangeAutoUpper(0,20,1.5);
+}
+
+void Sink::handleMessage(cMessage *msg)
+{
+  // Handle incoming packet
+  Packet *pk = check_and_cast<Packet *>(msg);
+  if (ev.isGUI()) parentModule()->bubble("Arrived");
+    numReceived++;
+
+  // Display the number of packets received.
+  char buf[40];
+  sprintf(buf, "rcvd: %ld", numReceived);
+  displayString().setTagArg("t", 0, buf);
+
+  //ev << simTime() << " received packet " << pk->name() << " after " << pk->getHopCount() << "hops" << endl;
+  //ev << simTime() << " received packet " << pk->name() << " after " << pk->getHopCount() << "hops" << endl;
+  pkDelayVector.record(simTime() - pk->timestamp());
+  pkDelayStats.collect(simTime() - pk->timestamp());
+  pkSizeStats.collect(pk->length());
+
+  delete pk;
+}
+
+void Sink::finish()
+{
+  ev << parentModule()->fullName() << "." << fullName() << ":" << endl;
+  ev << "  pks. received: " << numReceived << endl;
+
+  ev << "  hop count, min:    " << hopCountStats.min() << endl;
+  ev << "  hop count, max:    " << hopCountStats.max() << endl;
+  ev << "  hop count, mean:   " << hopCountStats.mean() << endl;
+  ev << "  hop count, stddev: " << hopCountStats.stddev() << endl;
+
+  ev << "  pk. delay, min:    " << pkDelayStats.min() << endl;
+  ev << "  pk. delay, max:    " << pkDelayStats.max() << endl;
+  ev << "  pk. delay, mean:   " << pkDelayStats.mean() << endl;
+  ev << "  pk. delay, stddev: " << pkDelayStats.stddev() << endl;
+
+  ev << "  pk. size, min:    " << pkSizeStats.min() << endl;
+  ev << "  pk. size, max:    " << pkSizeStats.max() << endl;
+  ev << "  pk. size, mean:   " << pkSizeStats.mean() << endl;
+  ev << "  pk. size, stddev: " << pkSizeStats.stddev() << endl;
+  ev << " " << endl;
+
+  recordScalar("#received", numReceived);
+  hopCountStats.recordScalar("hop count");
+  pkDelayStats.recordScalar("pk delay");
+  pkSizeStats.recordScalar("pk size");
+}
+
