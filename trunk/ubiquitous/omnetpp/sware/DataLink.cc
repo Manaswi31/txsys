afb850a0d9e4114ab160cb3fe5d8f5dd6c03147c
Illidan.Modeler <Illidan.Modeler@31749a9c-224c-0410-b4e1-a76a6b973bc7>
2008-07-03 16:03:20 +0000

    Core dump at event 128.

--- trunk/ubiquitous/omnetpp/sware/DataLink.cc
+++ trunk/ubiquitous/omnetpp/sware/DataLink.cc
@@ -21,27 +21,48 @@ void DataLink::handleMessage(cMessage *msg)
 	    appPkQueue.push_back(pendingPk);
 	else {
 	    AppPacket* copyPk = new AppPacket(*pendingPk);
-	    send(copyPk , "to_ll");
+	    SwareDataPk * swareDataPk = new SwareDataPk(); 
+	    swareDataPk->encapsulate(copyPk);
+	    send(swareDataPk , "to_ll");
 	    ackWaitTimer= new cMessage();
 	    scheduleAt(simulation.simTime()+ackWaitDuration, ackWaitTimer);
 	}
     } else if (msg->arrivalGateId()==gate("from_ll")->id()) {
 	/* This is a packet from lower layer*/
-	if (dynamic_cast<AppPacket*>(msg) != NULL) {
+	if (dynamic_cast<SwareDataPk*>(msg) != NULL) {
+	    /*This is a Data packet*/
 	    SwareDataPk* inPacket = check_and_cast <SwareDataPk*> (msg);
-	    send(inPacket, "to_hl");
+	    AppPacket* appPk = check_and_cast <AppPacket*> (inPacket->decapsulate());
+	    send(appPk, "to_hl");
+	    delete inPacket;
+
+	    /*Reply with an ACK*/
 	    SwareAck *ackPacket = new SwareAck(); 
 	    send(ackPacket, "to_ll");
 	} else {
 	    /* This is an ACK*/
-	    if (ackWaitTimer) 
+	    SwareAck* ack = check_and_cast <SwareAck*> (msg);
+	    delete ack;
+	    if (ackWaitTimer) {
 		cancelAndDelete(ackWaitTimer);
+		if (pendingPk)
+		    delete pendingPk;
+	    }
 	    if (!appPkQueue.empty()) {
 		AppPacket* outPacket = appPkQueue.front();
 	        appPkQueue.pop_front();
 		send(outPacket, "to_ll");
 	    }
 	}
+    }
+    else if (msg->isSelfMessage()) {
+	/*ACK timeout*/
+	SwareDataPk * swareDataPk = new SwareDataPk(); 
+	swareDataPk->encapsulate(pendingPk);
+	send(swareDataPk , "to_ll");
+	ackWaitTimer= new cMessage();
+	scheduleAt(simulation.simTime()+ackWaitDuration, ackWaitTimer);
+
     }
 }
 
