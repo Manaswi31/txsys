dc0caa56805f9c5487afa4d4e75329feab8e5d6f
illidan.modeler <illidan.modeler@31749a9c-224c-0410-b4e1-a76a6b973bc7>
2008-11-29 14:19:51 +0000

    Add a otcl template for wireless scnarios

--- trunk/ubiquitous/ns2/loss-monitor.tcl
+++ trunk/ubiquitous/ns2/loss-monitor.tcl
@@ -1,13 +1,20 @@
 #
+set basename loss-monitor
+
+set statIntvl 1.0
+set cbrIntvl 1.0
+
 #Initialize and create output files
 #Create a simulator instance
 set ns [new Simulator]
 
 #Crate a trace file and animation record
-set tracefd [open example1.tr w]
+set tracefd [open $basename.tr w]
 $ns trace-all $tracefd
-set namtracefd [open example1.nam w]
+set namtracefd [open $basename.nam w]
 $ns namtrace-all $namtracefd
+
+set outfd [open $basename.out w]
 
 #
 #Create Topology
@@ -25,38 +32,44 @@ $ns attach-agent $n0 $udp0
 # Create a CBR traffic source on node0
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 set packetSize_ 500
-$cbr0 set interval_ 0.005
+$cbr0 set interval_ $cbrIntvl
 $cbr0 set random_ 1
 $cbr0 attach-agent $udp0
 
 #Create a Null agent (a traffic sink) on node1
-set sink [new Agent/Sink]
+set sink0 [new Agent/LossMonitor]
 $ns attach-agent $n1 $sink0
 
 #Connet source and dest Agents
 $ns connect $udp0 $sink0
 
 proc record {} {
-    global sink ns
+    global sink0 ns outfd statIntvl
+    set bytes [$sink0 set bytes_]
+    set now [$ns now]
+    puts $outfd $bytes
+    $sink0 set bytes_ 0
+    $ns at [expr $now+$statIntvl] "record"
 }
 
 #a procedure to close trace file and nam file
 proc finish {} {
 
-	global ns tracefd namtracefd
+	global ns tracefd namtracefd basename
 	$ns flush-trace
 
 	close $tracefd
 	close $namtracefd
 	
-	exec nam example1.nam &
+	exec nam $basename.nam &
 	exit 0
 }
 
 #
-#Specify Discrete Event Simulation here
+#Schedule trigger events
 
 #Schedule events for the CBR agent that starts at 0.5s and stops at 4.5s
+$ns at 0.5 "record"
 $ns at 0.5 "$cbr0 start"
 $ns at 4.5 "$cbr0 stop"
 
