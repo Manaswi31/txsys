6ac2fe14d5d9c384fdb54f92f46cd135c6a06c26
jiangbei.wei@gmail.com <jiangbei.wei@gmail.com@31749a9c-224c-0410-b4e1-a76a6b973bc7>
2010-07-19 16:06:42 +0000

    before applying "long line" method

--- trunk/ubiquitous/octave/file_parser/lls_file_parse.m
+++ trunk/ubiquitous/octave/file_parser/lls_file_parse.m
@@ -2,11 +2,11 @@
 function [coeff_f3, coeff_f4, coeff_f5, f4_x1, f4_x2, f5_x1, f5_x2, f3_x1, f3_x2] = lls_file_parse(fp_source)
 
 line_counter = 0;
-curve_counter = 1;
 
         inblock_f4 = 0;
         inblock_f5 = 0;
         inblock_f3 = 0;
+	block = [];
 
 while(1)
         line = fgetl(fp_source);
@@ -27,21 +27,22 @@ while(1)
         if 1==inblock_f4
                 if 0==isempty(strfind (line, 'end_CirToSnr_F4'))
                         inblock_f4 = 0;
-                        continue;
-                else
-                        [coeff_f4_temp, f4_x1_inter_temp, f4_x2_inter_temp] = parse_f4(line);
+                        [coeff_f4_temp, f4_x1_inter_temp, f4_x2_inter_temp] = parse_f4(block);
                         if coeff_f4_temp~=0
                                 coeff_f4 = coeff_f4_temp;
                         end
 
                         if f4_x1_inter_temp~=0
-                                f4_x1_inter = f4_x1_inter_temp;
+                                f4_x1 = f4_x1_inter_temp;
                         end
 
                         if f4_x2_inter_temp~=0
-                                f4_x2_inter = f4_x2_inter_temp;
+                                f4_x2 = f4_x2_inter_temp;
                         end
 
+                       continue;
+                else
+		    block = [block line]; %%%contaminate to be a long line
                 end
         end
 
