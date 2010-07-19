9e320370021484ebedfbfab3654590aba0634bab
jiangbei.wei@gmail.com <jiangbei.wei@gmail.com@31749a9c-224c-0410-b4e1-a76a6b973bc7>
2010-07-19 16:07:25 +0000

    finished with parse_f4

--- trunk/ubiquitous/octave/file_parser/parse_f4.m
+++ trunk/ubiquitous/octave/file_parser/parse_f4.m
@@ -1,41 +1,35 @@
  	
 function [coeff_f4, x1_inter, x2_inter] =  parse_f4(line)
-%         if coeff_f4~=0
-%                 coeff_f4 = 0;
-%         end
-%         if x1_inter~=0
-%                 x1_inter = 0;
-%         end
-%         if x2_inter~=0
-%                 x2_inter = 0;
-%         end
     coeff_f4 = 0;
     x1_inter = 0;
     x2_inter = 0;
-    key = 'x1_inter';
-    pos = strfind(line, key);
-            x1_inter_str = line(pos+lengthkey:pos+8)
-            x1_inter = str2num(x1_inter_str);
-        end
 
-        if length(line)>8 && 1==strcmp('x2_inter', line(1:8))
-            x2_inter_str = line(11:end);
-            x2_inter = str2num(x2_inter_str);
-        end
+	key1 = 'x1_inter: ';
+	key2 = 'x2_inter: ';
+	pos1 = strfind(line, key1);
+	pos2 = strfind(line, key2);
+	x1_inter_str = line(pos1+length(key1):pos2-1);
+	x1_inter = str2num(x1_inter_str);
 
-        if length(line)>8 && 1==strcmp('x2_inter', line(1:8))
-            x2_inter_str = line(11:end);
-            x2_inter = str2num(x2_inter_str);
-        end
+	key1 = key2;
+	key2 = 'x0_intra: ';
+	pos1 = strfind(line, key1);
+	pos2 = strfind(line, key2);
+	x2_inter_str = line(pos1+length(key1):pos2-1);
+	x2_inter = str2num(x2_inter_str);
 
-        keystr = 'CIR_Inter:';
-        keylen = length(keystr);
-        if length(line)>keylen && 1==strcmp(keystr, line(1:keylen))
-            f4_coeff_str = line(keylen+1:end);
-            startpos = strfind(f4_coeff_str, ' ');
-            endpos = [strfind(f4_coeff_str, ',') length(f4_coeff_str)];
-            %coeff_f4 = [];
-            for ii=1:length(startpos)
-                    coeff_f4 (ii) = str2num(f4_coeff_str(startpos(ii):endpos(ii)));
-            end
+	key1 = 'CIR_Inter:';
+	key2 = 'CIR_Intra:';
+	pos1 = strfind(line, key1);
+	pos2 = strfind(line, key2);
+	f4_coeff_str = line(pos1+length(key1):pos2-1);
+	startpos = strfind(f4_coeff_str, ' ');
+	endpos = [strfind(f4_coeff_str, ',') length(f4_coeff_str)];
+	%coeff_f4 = [];
+	for ii=1:length(startpos)
+		coeff_f4 (ii) = str2num(f4_coeff_str(startpos(ii):endpos(ii)));
+	end
+%	x1_inter
+%	x2_inter
+%	coeff_f4
     end  %end of if
