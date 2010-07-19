11273256a07c573efd85ef7ecf766e7a780a8355
jiangbei.wei <jiangbei.wei@gmail.com>
2010-07-19 16:07:46 +0000

    Able to work with f5.

--- ubiquitous/octave/file_parser/lls_file_parse.m
+++ ubiquitous/octave/file_parser/lls_file_parse.m
@@ -47,65 +47,71 @@ while(1)
         end
 
         %%%%%
-
-        if 0==inblock_f5
+	if 0==inblock_f5
             if 0==isempty(strfind (line, 'start_SirToRawBer_F5'))
                 inblock_f5 = 1;
+		block = line;
+		continue;
             else
                 %continue;
             end
         end
-
+        
         if 1==inblock_f5
+		block = [block line] %%%contaminate to be a long line
                 if 0==isempty(strfind (line, 'end_SirToRawBer_F5'))
                         inblock_f5 = 0;
-                        continue;
-                else
-                        [coeff_f5_temp, f5_x1_temp, f5_x2_temp] = parse_f5(line);
+                        [coeff_f5_temp, f5_x1_inter_temp, f5_x2_inter_temp] = parse_f5(block);
                         if coeff_f5_temp~=0
                                 coeff_f5 = coeff_f5_temp;
                         end
 
-                        if f5_x1_temp~=0
-                                f5_x1 = f5_x1_temp;
+                        if f5_x1_inter_temp~=0
+                                f5_x1 = f5_x1_inter_temp;
                         end
 
-                        if f5_x2_temp~=0
-                                f5_x2 = f5_x2_temp;
+                        if f5_x2_inter_temp~=0
+                                f5_x2 = f5_x2_inter_temp;
                         end
+
+                       continue;
                 end
-        end
+	end
 
         %%%%%%%%%%%%%%%%%%%
         %Parsing F3%
         %%%%%%%%%%%%%%%%%%%
 
-        if 0==inblock_f3
+	if 0==inblock_f3
             if 0==isempty(strfind (line, 'start_RawBerToBler_F3'))
                 inblock_f3 = 1;
+		block = [];
             else
                 %continue;
             end
         end
-
-        if 1==inblock_f3
+        
+          if 1==inblock_f3
                 if 0==isempty(strfind (line, 'end_RawBerToBler_F3'))
                         inblock_f3 = 0;
-                        continue;
-                else
-                        [coeff_f3_temp, f3_x1_temp, f3_x2_temp] = parse_f3(line);
+                        [coeff_f3_temp, f3_x1_inter_temp, f3_x2_inter_temp] = parse_f3(block);
                         if coeff_f3_temp~=0
                                 coeff_f3 = coeff_f3_temp;
                         end
 
-                        if f3_x1_temp~=0
-                                f3_x1 = f3_x1_temp;
+                        if f3_x1_inter_temp~=0
+                                f3_x1 = f3_x1_inter_temp;
                         end
 
-                        if f3_x2_temp~=0
-                                f3_x2 = f3_x2_temp;
+                        if f3_x2_inter_temp~=0
+                                f3_x2 = f3_x2_inter_temp;
                         end
+
+                       continue;
+                else
+		    block = [block line]; %%%contaminate to be a long line
                 end
-        end
+	end
+
 end %end of outer while(1)
 
--- ubiquitous/octave/file_parser/parse_f5.m
+++ ubiquitous/octave/file_parser/parse_f5.m
@@ -1,36 +1,32 @@
  	
 function [coeff_f5, x1, x2] =  parse_f5(line)
-%         if coeff_f5~=0
-%                 coeff_f5 = 0;
-%         end
-%         if x1~=0
-%                 x1 = 0;
-%         end
-%         if x2~=0
-%                 x2 = 0;
-%         end
     coeff_f5 = 0;
     x1 = 0;
     x2 = 0;
-        if length(line)>5 && 1==strcmp('x0:', line(1:3))
-            x1_str = line(5:end);
-            x1 = str2num(x1_str);
-        end
 
-        if length(line)>5 && 1==strcmp('x1:', line(1:3))
-            x2_str = line(5:end);
-            x2 = str2num(x2_str);
-        end
+	key1 = 'x0: ';
+	key2 = 'x1: ';
+	pos1 = strfind(line, key1);
+	pos2 = strfind(line, key2);
+	x1_str = line(pos1+length(key1):pos2-1);
+	x1 = str2num(x1_str);
 
-        keystr = 'sir:';
-        keylen = length(keystr);
-        if length(line)>keylen && 1==strcmp(keystr, line(1:keylen))
-            f5_coeff_str = line(keylen+1:end);
-            startpos = strfind(f5_coeff_str, ' ');
-            endpos = [strfind(f5_coeff_str, ',') length(f5_coeff_str)];
-            %coeff_f5 = [];
-            for ii=1:length(startpos)
-                    coeff_f5 (ii) = str2num(f5_coeff_str(startpos(ii):endpos(ii)));
-            end
+	key1 = key2;
+	key2 = 'sir: ';
+	pos1 = strfind(line, key1);
+	pos2 = strfind(line, key2);
+	x2_str = line(pos1+length(key1):pos2-1);
+	x2 = str2num(x2_str);
+
+	key1 = 'sir:';
+	key2 = 'end_SirToRawBer_F5';
+	pos1 = strfind(line, key1);
+	pos2 = strfind(line, key2);
+	f5_coeff_str = line(pos1+length(key1):pos2-1);
+	startpos = strfind(f5_coeff_str, ' ');
+	endpos = [strfind(f5_coeff_str, ',') length(f5_coeff_str)];
+	%coeff_f5 = [];
+	for ii=1:length(startpos)
+		coeff_f5 (ii) = str2num(f5_coeff_str(startpos(ii):endpos(ii)));
+	end
     end  %end of if
-
